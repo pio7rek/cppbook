23b70dd5d9747586efddc83cb5343ad3847e6d55
Piotr Polomski <pio7rek@gmail.com>
2013-10-28 17:13:53 +0100

    initial

---
+++ ksiazkacpp/src/ksiazkacpp.cpp
@@ -0,0 +1,48 @@
+/*
+ * ksiazkacpp.cpp
+ *
+ *  Created on: 28 paü 2013
+ *      Author: piot
+ */
+
+#include <iostream>
+#include <istream>
+#include "rozdzial1/Vector.h"
+
+void vector_test() {
+	piot::Vector v(10);
+	for (int i=0; i!=v.size(); ++i) {
+		v[i] = i*2;
+		std::cout << v[i] << ' ';
+	}
+	std::cout << std::endl;
+}
+
+void v_initializer_test() {
+	piot::Vector v{2, 4, 6, 8, 10};
+	for (int i=0; i!=v.size(); ++i) {
+		v[i] = i*2;
+		std::cout << v[i] << ' ';
+	}
+	std::cout << std::endl;
+
+}
+
+/*piot::Vector read(istream& is) {
+	piot::Vector v;
+	for (double d; is>>d; ) {
+		v.push_back(d);
+	}
+	return v;
+}*/
+
+int main() {
+	std::cout << "hello!" << std::endl;
+
+//	v_initializer_test();
+
+	return 0;
+}
+
+
+
---
+++ ksiazkacpp/src/rozdzial3/Container.cpp
@@ -0,0 +1,21 @@
+/*
+ * Container.cpp
+ *
+ *  Created on: 28 paü 2013
+ *      Author: piot
+ */
+
+#include "C:Container.h"
+
+namespace piot {
+
+Container::Container() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Container::~Container() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace piot */
